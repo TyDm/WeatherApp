c64c01ee4214bf4c6433ccf8f14124d01404fe5e
TyDm <ty_dm@mail.ru>
2025-05-12 18:11:12 +0300

    fix

--- app/src/main/java/com/tydm/weatherApp/data/weatherapi/CityResponse.kt
+++ app/src/main/java/com/tydm/weatherApp/data/weatherapi/CityResponse.kt
@@ -4,68 +4,45 @@ import com.google.gson.annotations.SerializedName
 
 data class CityResponse(
 
-	@field:SerializedName("Version")
-	val version: Int? = null,
+    @field:SerializedName("Key")
+    val key: String? = null,
 
-	@field:SerializedName("Key")
-	val key: String? = null,
+    @field:SerializedName("Type")
+    val type: String? = null,
 
-	@field:SerializedName("Type")
-	val type: String? = null,
+    @field:SerializedName("Rank")
+    val rank: Int? = null,
 
-	@field:SerializedName("Rank")
-	val rank: Int? = null,
+    @field:SerializedName("LocalizedName")
+    val localizedName: String? = null,
 
-	@field:SerializedName("LocalizedName")
-	val localizedName: String? = null,
+    @field:SerializedName("Country")
+    val country: CityCountry? = null,
 
-	@field:SerializedName("Region")
-	val region: Region? = null,
+    @field:SerializedName("AdministrativeArea")
+    val administrativeArea: AdministrativeAreaInfo? = null,
 
-	@field:SerializedName("Country")
-	val country: CityCountry? = null,
-
-	@field:SerializedName("AdministrativeArea")
-	val administrativeArea: AdministrativeAreaInfo? = null,
-
-	@field:SerializedName("TimeZone")
-	val timeZone: TimeZone? = null,
+    @field:SerializedName("TimeZone")
+    val timeZone: TimeZone? = null,
 )
+
 data class TimeZone(
 
-	@field:SerializedName("NextOffsetChange")
-	val nextOffsetChange: Any? = null,
-
-	@field:SerializedName("GmtOffset")
-	val gmtOffset: Int? = null,
-
-	@field:SerializedName("Code")
-	val code: String? = null,
-
-	@field:SerializedName("IsDaylightSaving")
-	val isDaylightSaving: Boolean? = null,
-
-	@field:SerializedName("Name")
-	val name: String? = null
+    @field:SerializedName("GmtOffset")
+    val gmtOffset: Int? = null
 )
 
 data class AdministrativeAreaInfo(
 
-	@field:SerializedName("LocalizedType")
-	val localizedType: String? = null,
+    @field:SerializedName("LocalizedType")
+    val localizedType: String? = null,
 
-	@field:SerializedName("LocalizedName")
-	val localizedName: String? = null,
+    @field:SerializedName("LocalizedName")
+    val localizedName: String? = null,
 )
 
 data class CityCountry(
 
-	@field:SerializedName("LocalizedName")
-	val localizedName: String? = null,
-)
-
-data class Region(
-
-	@field:SerializedName("LocalizedName")
-	val localizedName: String? = null,
+    @field:SerializedName("LocalizedName")
+    val localizedName: String? = null,
 )
