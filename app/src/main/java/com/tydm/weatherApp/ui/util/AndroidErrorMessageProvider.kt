01170822e7b05cdbe1f3a657785e28d0426849a6
TyDm <ty_dm@mail.ru>
2025-06-06 11:03:29 +0300

    fix error message

--- app/src/main/java/com/tydm/weatherApp/ui/util/AndroidErrorMessageProvider.kt
+++ app/src/main/java/com/tydm/weatherApp/ui/util/AndroidErrorMessageProvider.kt
@@ -16,15 +16,7 @@ class AndroidErrorMessageProvider @Inject constructor(
     override fun getMessage(error: WeatherError): String {
         return when (error) {
             is WeatherError.NetworkError -> context.getString(R.string.error_network)
-            is WeatherError.ApiError -> when (error.code) {
-                400 -> context.getString(R.string.error_api_400)
-                401 -> context.getString(R.string.error_api_401)
-                403 -> context.getString(R.string.error_api_403)
-                404 -> context.getString(R.string.error_api_404)
-                429 -> context.getString(R.string.error_api_429)
-                500 -> context.getString(R.string.error_api_500)
-                else -> context.getString(R.string.error_server, error.message)
-            }
+            is WeatherError.ApiError -> context.getString(R.string.error_api, error.code.toString())
             is WeatherError.DatabaseError -> context.getString(R.string.error_database)
             is WeatherError.LocationError -> error.message
             is WeatherError.UnknownError -> context.getString(R.string.error_unknown)
