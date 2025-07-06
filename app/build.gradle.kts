4085c865544e6c4bbc107d2535d221b001095427
TyDm <ty_dm@mail.ru>
2025-07-06 12:25:14 +0300

    refactor build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("androidx.room")
-    alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
     id("com.android.internal.application")
+    alias(libs.plugins.ksp)
 }
 
 val localProperties = Properties().apply {
